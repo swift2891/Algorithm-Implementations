884fe56e4c792180d8e3d9487b158a666e03ebc3
Vigneshwar Padmanaban <swift2891@gmail.com>
2017-10-26 22:47:20 -0400

    Mergesort in progress

---
+++ MergeSort.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+/**
+ *
+ * @author Vignesh
+ */
+public class MergeSort {
+    public static void main(String[] args) {
+       	Scanner input = new Scanner(System.in);
+		int[] inputArray;
+		int i=0;
+                int arrayLength = input.nextInt();
+                inputArray = new int[arrayLength];
+		for(i=0;i<arrayLength;i++){
+		    inputArray[i] = input.nextInt();  
+ 	 	}
+	}
+}
