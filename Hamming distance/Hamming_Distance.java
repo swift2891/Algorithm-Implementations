a5fbf7e90c616cd850b0a9955e8feb9ce4e6aede
Vigneshwar Padmanaban <swift2891@gmail.com>
2017-11-05 21:35:27 -0500

    Added new problem - Hamming dist

---
+++ Hamming distance/Hamming_Distance.java
@@ -0,0 +1,19 @@
+public class Hamming_Distance{
+
+    public int hammingDistance(int x, int y) {
+    
+    	//Convert int to binary Strings
+
+    	//XOR the Strings and store in String Builder
+    	
+    	//Calculate the Distance from xor output
+
+
+    }
+
+	public static void main(String args[]){
+
+		Hamming_Distance hd = new Hamming_Distance();
+		System.out.println(hd.hammingDistance(1, 4));
+	}
+}
