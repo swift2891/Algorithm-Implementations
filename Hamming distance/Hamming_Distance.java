5d0270496569826d6bb58372936cd4962f42d78d
Vigneshwar Padmanaban <swift2891@gmail.com>
2017-12-17 21:38:13 -0500

    Completed Hamming Dist code

--- Hamming distance/Hamming_Distance.java
+++ Hamming distance/Hamming_Distance.java
@@ -1,19 +1,23 @@
 public class Hamming_Distance{
 
     public int hammingDistance(int x, int y) {
-    
-    	//Convert int to binary Strings
+    	String result;
+    	int dist=0;
 
-    	//XOR the Strings and store in String Builder
-    	
-    	//Calculate the Distance from xor output
+    	result = Integer.toBinaryString(x^y);
+    	String[] resultArray = result.split("");
 
-
+	   	//Calculate the Distance from xor output
+    	for(int i=0;i<resultArray.length;i++){
+    		if(Integer.parseInt(resultArray[i])==1)
+    			dist++;
+    	}
+    	return dist;
     }
 
 	public static void main(String args[]){
 
 		Hamming_Distance hd = new Hamming_Distance();
-		System.out.println(hd.hammingDistance(1, 4));
+		System.out.println(hd.hammingDistance(Integer.parseInt(args[0]), Integer.parseInt(args[1])));
 	}
 }
