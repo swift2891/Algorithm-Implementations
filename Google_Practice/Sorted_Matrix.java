dce411ed0c197bc5feb676f522bca003e9de83e3
Vigneshwar Padmanaban <swift2891@gmail.com>
2018-08-06 18:14:30 -0400

    Creating a new repo for DP problems

---
+++ Google_Practice/Sorted_Matrix.java
@@ -0,0 +1,12 @@
+/*
+Given a sorted matrix where the number below and right of you will always 
+be bigger, write an algorithm to find if a particular number exist in the 
+matrix. What is the running time of your algorithm
+*/
+
+public class Sorted_Matrix{
+	public static void main(String[] args){
+		int[][] matrix= new int[3][5];
+		System.out.println(matrix[0].length);
+	}
+}
